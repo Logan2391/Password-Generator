2fef81b71f3e3b6eb25b690fe86b0581c4d5fce1
Combatxd <logan.schoerner@gmail.com>
2022-07-04 15:59:29 -0600

    firstPrompt

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Assignment Code/Event Listener 
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+
+
+const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const calpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialC = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+function generatePassword() {
+  var length = prompt("Please input desired password length. Must be between 8-128 characters.");
+  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,6 +22,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
